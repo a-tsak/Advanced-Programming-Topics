51fc18cb9275a59b1c79eec21bf6fa286fa45e91
Savaskos <stosavas@hotmail.com>
2023-04-03 14:54:06 +0300

    Final Program

--- src/main/java/gr/uop/App.java
+++ src/main/java/gr/uop/App.java
@@ -9,6 +9,7 @@ import javafx.geometry.VPos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.Label;
 import javafx.scene.control.Separator;
 import javafx.scene.control.TextArea;
@@ -40,8 +41,8 @@ public class App extends Application {
         gSets.setCollapsible(false);
         vboxSets.getChildren().addAll(cb1, cb2);
         vboxSets.setSpacing(10);
-        gSets.setAlignment(Pos.CENTER);
-
+        gSets.setAlignment(Pos.TOP_CENTER);
+        
 
         //Grid Dimensions TitledPane
         VBox vboxDims = new VBox();
@@ -49,6 +50,8 @@ public class App extends Application {
         Label label2 = new Label("Vertical: ");
         TextField tf1 = new TextField();
         TextField tf2 = new TextField();
+        tf1.setPrefWidth(80);
+        tf2.setPrefWidth(80);
         HBox box1 = new HBox();
         box1.setAlignment(Pos.CENTER_RIGHT);
         box1.getChildren().addAll(label1, tf1);
@@ -59,8 +62,8 @@ public class App extends Application {
         gDims.setCollapsible(false);
         vboxDims.getChildren().addAll(box1, box2);
         vboxDims.setSpacing(10);
-        gDims.setAlignment(Pos.CENTER);
-
+        gDims.setAlignment(Pos.TOP_CENTER);
+        
 
 
 
@@ -68,33 +71,36 @@ public class App extends Application {
         VBox vboxButtons = new VBox();
         Button b1 = new Button("OK");
         Button b2 = new Button("Cancel");
-        Button b3 = new Button("Help");
-        b1.setPrefWidth(80);
-        b2.setPrefWidth(80);
-        b3.setPrefWidth(80);
+        Button b3 = new Button("Help"); 
+        b1.setPrefWidth(60);
+        b2.setPrefWidth(60);
+        b3.setPrefWidth(60);
         vboxButtons.getChildren().addAll(b1,b2,b3);
         vboxButtons.setSpacing(10);
-        vboxButtons.setAlignment(Pos.CENTER);
+        vboxButtons.setAlignment(Pos.TOP_CENTER);
 
 
         //Separator and label
         Separator s1 = new Separator(Orientation.HORIZONTAL);
         Label status = new Label("Status");
         VBox bottom = new VBox(s1,status);
-        bottom.setPadding(new Insets(0, 0, 0, 0));
         bottom.setAlignment(Pos.BOTTOM_LEFT);
-
+        status.setAlignment(Pos.BOTTOM_LEFT);
 
 
         //Grid properties
-        
-        grid.setPadding(new Insets(0,30,10,30));
-        grid.setAlignment(Pos.CENTER);
-        grid.setHgap(10);
         grid.setVgap(10);
+        grid.setHgap(10);
+        grid.setPadding(new Insets(10,30,10,30));
+        grid.setAlignment(Pos.CENTER);
+        GridPane.setHgrow(grid, Priority.ALWAYS);
+        GridPane.setVgrow(grid, Priority.ALWAYS);
         
+        
+       
 
         //Add elements to grid
+        
         grid.add(gSets, 0, 0);
         grid.add(gDims, 1, 0);
         grid.add(vboxButtons, 2, 0);
@@ -105,6 +111,8 @@ public class App extends Application {
         var scene = new Scene(grid, 500, 180);
         stage.setScene(scene);
         stage.setTitle("Settings");
+        stage.setMinWidth(500);
+        stage.setMinHeight(180); 
         stage.show();
     }
 
